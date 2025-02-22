f7345f4f0f5ed75e91ad6795edde73a110f6cbd8
Bryan Szalai <bszala1@wgu.edu>
2025-02-22 03:14:50 -0800

    Unused Code Removed

--- model_init.py
+++ model_init.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.preprocessing import StandardScaler
+
 
 RFC = None
 metrics = []
@@ -16,10 +16,6 @@ def train_model():
     y = df.values[:, 9]
 
     X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=40)
-
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
 
     global RFC
     RFC = RandomForestClassifier(class_weight="balanced", random_state=40)
